fb149cdd610ebd0f06c5538172c6d0bedfa8f96e
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-21 23:43:52 +0800

    finish startWith

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,7 @@
+// export default function collectCarNumberCount(collection) {
+//   return collection.filter(carNumber=>carNumber.s
+// }
+
 export default function collectCarNumberCount(collection) {
-  // TODO 7: 在这里写实现代码
+  return collection.filter(carNumber => carNumber.startsWith('粤A')).length;
 }
