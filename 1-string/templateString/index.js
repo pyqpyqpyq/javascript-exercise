b6ba07d4ca6c6d605c8425d67ec22acaba2c64a9
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-22 00:16:49 +0800

    getScoreOutput

--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -5,6 +5,19 @@ function getScore(id) {
   return 59;
 }
 
+// export default function getScoreOutput(person) {
+//   const grade = getScore(id);
+//   return `你好，${person.firstname} ${person.lastName}！你的考试成绩为${grade}分`;
+// }
+
+// export default function getScoreOutput(person) {
+//   const { firstName, lastName, id } = person;
+//   const grade = getScore(id);
+//   return `你好，${lastName}${firstName}！你的考试成绩为${grade}分`;
+// }
+
 export default function getScoreOutput(person) {
-  // TODO 8: 在这里写实现代码
+  // const { firstName, lastName, id } = person;
+  const grade = getScore(id);
+  return `你好，${person.lastName}${person.firstName}！你的考试成绩为${grade}分`;
 }
