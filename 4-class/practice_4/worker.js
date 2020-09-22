f87256d2412ac635abd8248fa62acbaf6eb11f84
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-09-22 14:14:34 +0800

    finished 4-class

--- 4-class/practice_4/worker.js
+++ 4-class/practice_4/worker.js
@@ -1,1 +1,12 @@
-// TODO 17: 在这里写实现代码
+import Person from './person';
+
+export default class Student extends Person {
+  constructor(name, age) {
+    super(name, age);
+  }
+
+  introduce() {
+    const personIntroduce = super.introduce();
+    return `${personIntroduce} I am a Worker. I have a job.`;
+  }
+}
--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,17 @@
-// TODO 20: 在这里写实现代码
+import Person from './person';
+
+export default class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    const personIntroduce = super.introduce();
+    const { klass } = this;
+    if (klass === null || klass === undefined) {
+      return `${personIntroduce} I am a Teacher. I teach No Class.`;
+    }
+    return `${personIntroduce} I am a Teacher. I teach Class ${klass}.`;
+  }
+}
